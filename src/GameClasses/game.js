40928dceae987c720a796b9de470ed132c1b38a9
Irina <i.liasota@ya.ru>
2025-02-13 20:30:34 +0300

    Second

--- src/GameClasses/game.js
+++ src/GameClasses/game.js
@@ -1,90 +1,90 @@
 import img_goblin from "C:\\Users\\erdya\\OneDrive\\Рабочий стол\\gnoms\\src\\img\\goblin.png";
 
 class CellHandler {
-    constructor(game) {
-        this.game = game;
-        this.cells = document.querySelectorAll('.cell');
-        this.init();
-    }
+  constructor(game) {
+    this.game = game;
+    this.cells = document.querySelectorAll(".cell");
+    this.init();
+  }
 
-    init() {
-        this.cells.forEach(cell => {
-            cell.addEventListener('click', (event) => this.handleClick(event));
-        });
-    }
+  init() {
+    this.cells.forEach((cell) => {
+      cell.addEventListener("click", (event) => this.handleClick(event));
+    });
+  }
 
-    handleClick(event) {
-        const img = event.currentTarget.querySelector('img');
-        if (img && img.style.display !== 'none') {
-            img.style.display = 'none';
-            event.currentTarget.classList.remove('cell-with-goblin');
-            this.game.incrementDefeated();
-        }
+  handleClick(event) {
+    const img = event.currentTarget.querySelector("img");
+    if (img && img.style.display !== "none") {
+      img.style.display = "none";
+      event.currentTarget.classList.remove("cell-with-goblin");
+      this.game.incrementDefeated();
     }
+  }
 }
 
 export class Game {
-    constructor() {
-        this.missed = 0;
-        this.defeated = 0;
-        this.cells = document.querySelectorAll('.cell');
-        this.cellHandler = new CellHandler(this);
-        this.img_goblin = img_goblin;
-        this.init();
+  constructor() {
+    this.missed = 0;
+    this.defeated = 0;
+    this.cells = document.querySelectorAll(".cell");
+    this.cellHandler = new CellHandler(this);
+    this.img_goblin = img_goblin;
+    this.init();
+  }
+
+  init() {
+    this.moveImage();
+    setInterval(() => this.moveImage(), 1000);
+  }
+
+  moveImage() {
+    let currentCellWithImage = null;
+
+    this.cells.forEach((cell, index) => {
+      const img = cell.querySelector("img");
+      if (img && img.style.display !== "none") {
+        currentCellWithImage = index;
+      }
+    });
+
+    if (currentCellWithImage !== null) {
+      const currentImg = this.cells[currentCellWithImage].querySelector("img");
+      currentImg.style.display = "none";
+      this.cells[currentCellWithImage].classList.remove("cell-with-goblin");
+      this.incrementMissed();
     }
 
-    init() {
-        this.moveImage();
-        setInterval(() => this.moveImage(), 1000);
+    let randomIndex;
+    do {
+      randomIndex = Math.floor(Math.random() * this.cells.length);
+    } while (randomIndex === currentCellWithImage);
+
+    const newImg = this.cells[randomIndex].querySelector("img");
+    if (newImg) {
+      newImg.style.display = "block";
+    } else {
+      const img = document.createElement("img");
+      img.src = this.img_goblin;
+      img.style.display = "block";
+      this.cells[randomIndex].appendChild(img);
     }
 
-    moveImage() {
-        let currentCellWithImage = null;
+    this.cells[randomIndex].classList.add("cell-with-goblin");
+  }
 
-        this.cells.forEach((cell, index) => {
-            const img = cell.querySelector('img');
-            if (img && img.style.display !== 'none') {
-                currentCellWithImage = index;
-            }
-        });
+  incrementMissed() {
+    this.missed++;
+    document.getElementById("missed").textContent = this.missed;
 
-        if (currentCellWithImage !== null) {
-            const currentImg = this.cells[currentCellWithImage].querySelector('img');
-            currentImg.style.display = 'none';
-            this.cells[currentCellWithImage].classList.remove('cell-with-goblin');
-            this.incrementMissed();
-        }
-
-        let randomIndex;
-        do {
-            randomIndex = Math.floor(Math.random() * this.cells.length);
-        } while (randomIndex === currentCellWithImage);
-
-        const newImg = this.cells[randomIndex].querySelector('img');
-        if (newImg) {
-            newImg.style.display = 'block';
-        } else {
-            const img = document.createElement('img');
-            img.src = this.img_goblin;
-            img.style.display = 'block';
-            this.cells[randomIndex].appendChild(img);
-        }
-
-        this.cells[randomIndex].classList.add('cell-with-goblin');
+    if (this.missed >= 5) {
+      alert("Игра закончена! Вы пропустили 5 гномов.");
+      window.location.reload();
     }
+  }
 
-    incrementMissed() {
-        this.missed++;
-        document.getElementById('missed').textContent = this.missed;
-
-        if (this.missed >= 5) {
-            alert('Игра закончена! Вы пропустили 5 гномов.');
-            window.location.reload();
-        }
-    }
-
-    incrementDefeated() {
-        this.defeated++;
-        document.getElementById('defeated').textContent = this.defeated;
-    }
+  incrementDefeated() {
+    this.defeated++;
+    document.getElementById("defeated").textContent = this.defeated;
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./css/style.css";
 import "./js/app";
 
-import { Game } from './GameClasses/game'
+import { Game } from "./GameClasses/game";
 
 // TODO: write your code in app.js
 
