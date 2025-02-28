6eb837e050643f2b022cd21a455c879be4f60fb9
illyr <lysota2014@yandex.ru>
2025-02-28 14:52:00 +0300

    5th

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./css/style.css";
 import "./js/app";
-import { Game } from "./GameClasses/Game.js";
+import { Game } from "./GameClasses/game.js";
 import { CellHandler } from "./GameClasses/CellHandler.js";
 
 // // TODO: write your code in app.js
